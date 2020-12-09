779e4a98186c59940bd745d75bb6044d6f23fe14
afcmrp <hjelm.emil@gmail.com>
2020-12-09 23:54:29 +0100

    Cleaner 4

--- 2020/4.py
+++ 2020/4.py
@@ -71,25 +71,11 @@ def valid_passport2(passport):
                 return False
     return True
 
-def valid_passports1(passports):
-    valid = 0
-    for passport in passports:
-        if valid_passport1(passport):
-            valid += 1
-    return valid
-
-def valid_passports2(passports):
-    valid = 0
-    for passport in passports:
-        if valid_passport2(passport):
-            valid += 1
-    return valid
-
 def main():
     passports = get_passports()
-    valid1 = valid_passports1(passports)
+    valid1 = sum([valid_passport1(p) for p in passports])
     print("Part 1: " + str(valid1))
-    valid2 = valid_passports2(passports)
+    valid2 = sum([valid_passport2(p) for p in passports])
     print("Part 2: " + str(valid2))
 
 if __name__ == "__main__":
